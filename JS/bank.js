c571f33fea94ef4b175608e2582c540dbcf63c46
Mohammad Yeasin Arafat Fahim <fahim.std77@gmail.com>
2021-08-15 15:39:46 +0600

    deposit withdraw balance

--- JS/bank.js
+++ JS/bank.js
@@ -1,10 +1,14 @@
 document.getElementById('login-submit').addEventListener('click', function () {
+
+    //get user email
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
 
-    //get use password
+    //get useer password
     const passwordField = document.getElementById('user-password');
     const userPassword = passwordField.value;
+
+    //if check email and password
     if (userEmail == 'abc@gmail.com' && userPassword == 'secret') {
         window.location.href = 'banking.html'
 
